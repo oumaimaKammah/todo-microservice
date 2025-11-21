a62c990516e0de4ba2ac105333d0451c5caa89fe
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2025-11-21 11:27:20 +0100

    [#4] feat(Validation) : Ajout des contraintes de validation sur Task (@NotBlank, @Size, @Pattern, @NotNull)

--- src/main/java/com/model/Task.java
+++ src/main/java/com/model/Task.java
@@ -1,6 +1,9 @@
 package com.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Setter
@@ -18,9 +21,15 @@ public class Task {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotBlank(message = "Le titre est obligatoire")
+    @Size(min = 3, max = 100, message = "Le titre doit contenir entre 3 et 100 caractères")
     private String title;
+
+    @Size(max = 500, message = "La description ne peut pas dépasser 500 caractères")
     private String description;
 
+    @NotBlank(message = "Le statut est obligatoire")
+    @Pattern(regexp = "Pending|In Progress|Completed", message = "Le statut doit être 'Pending'n 'In Progress' ou 'Completed'")
     @Column(nullable = false)
     private String status;
 
